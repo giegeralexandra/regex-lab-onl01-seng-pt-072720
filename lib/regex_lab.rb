8c66fc584aa76fc8f15c4fc6acd5735e496c78ac
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-11 11:30:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/) 
+    true
+  else 
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  return_array = text.split(" ")
+  return_array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.start_with?(/[A..Z]/) && text.match(/[?!.]$/)
+    true
+  else 
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/(\d{3})-(\d{3})-(\d{3})/)
 end
